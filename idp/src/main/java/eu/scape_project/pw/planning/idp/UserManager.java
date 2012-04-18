f72947386ab2899de3644d13d9b6072436bfff5e
Markus Hamm <hamm@ifs.tuwien.ac.at>
2012-04-18 11:26:59 +0200

    Added UserManager class

---
+++ idp/src/main/java/eu/scape_project/pw/planning/idp/UserManager.java
@@ -0,0 +1,22 @@
+package eu.scape_project.pw.planning.idp;
+
+import javax.ejb.Stateful;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+
+import org.slf4j.Logger;
+
+@Stateful
+@SessionScoped
+@Named("userManager")
+public class UserManager {
+
+	@Inject
+	private Logger log;
+
+	@Inject
+	private EntityManager em;
+
+}
---
+++ idp/src/main/java/eu/scape_project/pw/planning/idp/bean/RegisterUserBean.java
@@ -0,0 +1,9 @@
+package eu.scape_project.pw.planning.idp.bean;
+
+public class RegisterUserBean {
+
+	public RegisterUserBean() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
