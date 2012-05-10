37b455668bff0516f32ec011603fa3556226b48b
Markus Hamm <hamm@ifs.tuwien.ac.at>
2012-05-10 13:08:40 +0200

    re-formatted UserManagerTest

--- idp/src/main/java/eu/scape_project/pw/idp/bean/AddUserBean.java
+++ idp/src/main/java/eu/scape_project/pw/idp/bean/AddUserBean.java
@@ -23,107 +23,109 @@ import eu.scape_project.pw.idp.model.IdpUser;
 @ViewScoped
 public class AddUserBean {
 
-  @Inject
-  private EntityManager em;
+    @Inject
+    private EntityManager em;
 
-  @Inject
-  private Logger log;
+    @Inject
+    private Logger log;
 
-  private IdpUser user;
+    private IdpUser user;
 
-  private ReCaptcha reCaptcha;
+    private ReCaptcha reCaptcha;
 
-  private Boolean addUserSuccessful;
+    private Boolean addUserSuccessful;
 
-  private Boolean activateUserSuccessful;
+    private Boolean activateUserSuccessful;
 
-  /**
-   * Temporary (dummy) variable only required to map the recaptchaHelper jsf-input-tag to an value (which is mandatory).
-   * Thus, this field has no impact on this class functionality.
-   */
-  private String recaptchaHelper;
+    /**
+     * Temporary (dummy) variable only required to map the recaptchaHelper
+     * jsf-input-tag to an value (which is mandatory). Thus, this field has no
+     * impact on this class functionality.
+     */
+    private String recaptchaHelper;
 
-  @Inject
-  private UserManager userManager;
+    @Inject
+    private UserManager userManager;
 
-  public AddUserBean() {
-    user = new IdpUser();
+    public AddUserBean() {
+        user = new IdpUser();
 
-    reCaptcha = ReCaptchaFactory.newReCaptcha("6Lclf9ASAAAAAJE2REWGZ7chcFgndfWIhAY01v_n",
-      "6Lclf9ASAAAAAGMQlB8N-6a-UeKKXH6eMuB1hnEH", false);
-    addUserSuccessful = false;
-    activateUserSuccessful = false;
-  }
-
-  public void addUser() {
-    // create user
-    userManager.addUser(user);
-    addUserSuccessful = true;
-
-    // send user activation mail
-    HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext()
-      .getRequest();
-    String serverString = request.getServerName() + ":" + request.getServerPort();
-    userManager.sendActivationMail(user, serverString);
-  }
-
-  /**
-   * Method responsible for triggering the activation of an already created user
-   * 
-   * @return outcome-string of the page to display.
-   */
-  public void activateUser() {
-    // fetch the token from URL request
-    HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext()
-      .getRequest();
-    String actionToken = request.getParameter("uid");
-
-    if (actionToken != null) {
-      activateUserSuccessful = userManager.activateUser(actionToken);
-    } else {
-      activateUserSuccessful = false;
+        reCaptcha = ReCaptchaFactory.newReCaptcha("6Lclf9ASAAAAAJE2REWGZ7chcFgndfWIhAY01v_n",
+            "6Lclf9ASAAAAAGMQlB8N-6a-UeKKXH6eMuB1hnEH", false);
+        addUserSuccessful = false;
+        activateUserSuccessful = false;
     }
-  }
 
-  // ---------- getter/setter ----------
+    public void addUser() {
+        // create user
+        userManager.addUser(user);
+        addUserSuccessful = true;
 
-  public IdpUser getUser() {
-    return user;
-  }
+        // send user activation mail
+        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext()
+            .getRequest();
+        String serverString = request.getServerName() + ":" + request.getServerPort();
+        userManager.sendActivationMail(user, serverString);
+    }
 
-  public void setUser(IdpUser user) {
-    this.user = user;
-  }
+    /**
+     * Method responsible for triggering the activation of an already created
+     * user
+     * 
+     * @return outcome-string of the page to display.
+     */
+    public void activateUser() {
+        // fetch the token from URL request
+        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext()
+            .getRequest();
+        String actionToken = request.getParameter("uid");
 
-  public ReCaptcha getReCaptcha() {
-    return reCaptcha;
-  }
+        if (actionToken != null) {
+            activateUserSuccessful = userManager.activateUser(actionToken);
+        } else {
+            activateUserSuccessful = false;
+        }
+    }
 
-  public void setReCaptcha(ReCaptcha reCaptcha) {
-    this.reCaptcha = reCaptcha;
-  }
+    // ---------- getter/setter ----------
 
-  public Boolean getActivateUserSuccessful() {
-    return activateUserSuccessful;
-  }
+    public IdpUser getUser() {
+        return user;
+    }
 
-  public void setActivateUserSuccessful(Boolean activateUserSuccessful) {
-    this.activateUserSuccessful = activateUserSuccessful;
-  }
+    public void setUser(IdpUser user) {
+        this.user = user;
+    }
 
-  public String getRecaptchaHelper() {
-    return recaptchaHelper;
-  }
+    public ReCaptcha getReCaptcha() {
+        return reCaptcha;
+    }
 
-  public void setRecaptchaHelper(String recaptchaHelper) {
-    this.recaptchaHelper = recaptchaHelper;
-  }
+    public void setReCaptcha(ReCaptcha reCaptcha) {
+        this.reCaptcha = reCaptcha;
+    }
 
-  public Boolean getAddUserSuccessful() {
-    return addUserSuccessful;
-  }
+    public Boolean getActivateUserSuccessful() {
+        return activateUserSuccessful;
+    }
 
-  public void setAddUserSuccessful(Boolean addUserSuccessful) {
-    this.addUserSuccessful = addUserSuccessful;
-  }
+    public void setActivateUserSuccessful(Boolean activateUserSuccessful) {
+        this.activateUserSuccessful = activateUserSuccessful;
+    }
+
+    public String getRecaptchaHelper() {
+        return recaptchaHelper;
+    }
+
+    public void setRecaptchaHelper(String recaptchaHelper) {
+        this.recaptchaHelper = recaptchaHelper;
+    }
+
+    public Boolean getAddUserSuccessful() {
+        return addUserSuccessful;
+    }
+
+    public void setAddUserSuccessful(Boolean addUserSuccessful) {
+        this.addUserSuccessful = addUserSuccessful;
+    }
 }
