4d1c8e693bfd94b1d12a6ecc1251dcfda8c26c89
Michael Kraxner <kraxner@ifs.tuwien.ac.at>
2012-08-09 14:33:03 +0200

    component lookup integrated in plato #55
    * component registry queries myExperiment - sparql endpoint, and creates PreservationActionDefinitions from the response
    * added ComponentRegistry to PreservationActionRegistries.xml

--- plato/src/main/java/eu/scape_project/planning/services/pa/taverna/ComponentRegistry.java
+++ plato/src/main/java/eu/scape_project/planning/services/pa/taverna/ComponentRegistry.java
@@ -1,13 +1,23 @@
 package eu.scape_project.planning.services.pa.taverna;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.MalformedURLException;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.rpc.ServiceException;
 
+import org.apache.commons.digester3.Digester;
+import org.apache.commons.digester3.RegexRules;
+import org.apache.commons.digester3.SimpleRegexMatcher;
 import org.apache.log4j.Logger;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
 
 import eu.scape_project.planning.model.FormatInfo;
 import eu.scape_project.planning.model.PlatoException;
@@ -20,6 +30,8 @@ public class ComponentRegistry implements IPreservationActionRegistry {
 
 	private static final String ME_SPARQL_ENDPOINT = "http://rdf.myexperiment.org/sparql";
 	private static final String ENCODING_UTF8 = "URF-8";
+	
+	private List<PreservationActionDefinition>  preservationActions;
 
 	@Override
 	public void connect(String URL) throws ServiceException,
@@ -54,6 +66,8 @@ ORDER BY ?w ?wt
 	@Override
 	public List<PreservationActionDefinition> getAvailableActions(
 			FormatInfo sourceFormat) throws PlatoException {
+		preservationActions = new ArrayList<PreservationActionDefinition>();
+		
 		StringBuilder query = new StringBuilder();
 		query.append("PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>").append("\n")
 			 .append("PREFIX dcterms: <http://purl.org/dc/terms/>").append("\n")
@@ -69,21 +83,24 @@ ORDER BY ?w ?wt
 			 .append("  ?tscape meannot:uses-tag <http://www.myexperiment.org/tags/2681> .").append("\n")
 			 .append("  ?tmigration meannot:uses-tag <http://www.myexperiment.org/tags/3108> .").append("\n")
 			 .append("}").append("\n")
-			 .append("ORDER BY ?w ?wt").append("\n");
+			 .append("ORList<PreservationActionDefinition>DER BY ?w ?wt").append("\n");
 		try {
 			String url = ME_SPARQL_ENDPOINT + "?query=" + URLEncoder.encode(query.toString(), ENCODING_UTF8) + "&formatting=XML&reasoning=1";
 			String response = JGet.wget(url);
 			log.debug(response);
+			
+			new SparqlResultComponentsParser().addComponentsFromSparqlResult(preservationActions, new StringReader(response));
+
+			return preservationActions;
+			
 		} catch (MalformedURLException e) {
 			throw new PlatoException("Component registry is not configured properly.",e);
 		} catch (IOException e) {
 			throw new PlatoException("Failed to retrieve list of components.",e);
 		}
-		
-		
-		return null;
-		
 	}
+	
+
 
 
 
---
+++ plato/src/main/java/eu/scape_project/planning/services/pa/taverna/SparqlResultComponentsParser.java
@@ -0,0 +1,57 @@
+package eu.scape_project.planning.services.pa.taverna;
+
+import java.io.Reader;
+import java.util.HashMap;
+import java.util.List;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+import org.dom4j.XPath;
+import org.dom4j.io.SAXReader;
+
+import eu.scape_project.planning.model.PlatoException;
+import eu.scape_project.planning.model.PreservationActionDefinition;
+
+public class SparqlResultComponentsParser {
+	
+	
+	public void addComponentsFromSparqlResult(List<PreservationActionDefinition> components, Reader sparqlResult) throws PlatoException {
+		
+		
+		SAXReader reader = new SAXReader();
+		try {
+			Document sparqlDoc = reader.read(sparqlResult);
+			
+			HashMap<String, String> map = new HashMap<String, String>();
+			map.put( "sparql", "http://www.w3.org/2005/sparql-results#");
+			  
+			XPath xpath = DocumentHelper.createXPath("sparql:results/*");
+			XPath selectUrlXpath = DocumentHelper.createXPath("sparql:binding[@name='w']/sparql:uri");
+			XPath selectTitleXpath = DocumentHelper.createXPath("sparql:binding[@name='wt']/sparql:literal");
+			XPath selectDescriptionXpath = DocumentHelper.createXPath("sparql:binding[@name='wdesc']/sparql:literal");
+
+			xpath.setNamespaceURIs(map);
+			selectUrlXpath.setNamespaceURIs(map);
+			selectTitleXpath.setNamespaceURIs(map);
+			selectDescriptionXpath.setNamespaceURIs(map);
+			
+			@SuppressWarnings("rawtypes")
+			List componentsNodes = xpath.selectNodes(sparqlDoc.getRootElement());
+			
+			for (int i = 0; i < componentsNodes.size(); i++) {
+				Element component = (Element)componentsNodes.get(i);
+				PreservationActionDefinition def = new PreservationActionDefinition();
+
+				def.setUrl(selectUrlXpath.selectSingleNode(component).getText());
+				def.setInfo(selectTitleXpath.selectSingleNode(component).getText());
+				def.setDescriptor(selectDescriptionXpath.selectSingleNode(component).getText());
+				
+				components.add(def);
+			}
+		} catch (DocumentException e) {
+			throw new PlatoException("An error occured while reading the sparql result: {}", e);
+		}
+	}
+}
