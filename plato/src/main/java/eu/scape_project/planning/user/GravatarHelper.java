269ce36688c58e99fb025671cd90e6ba6853abb4
Markus <plangg@ifs.tuwien.ac.at>
2012-08-17 20:28:17 +0200

    GravatarHelper returns default image if email is null

--- plato/src/main/java/eu/scape_project/planning/user/GravatarHelper.java
+++ plato/src/main/java/eu/scape_project/planning/user/GravatarHelper.java
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * Copyright 2006 - 2012 Vienna University of Technology,
+ * Department of Software Technology and Interactive Systems, IFS
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package eu.scape_project.planning.user;
 
 import java.io.Serializable;
@@ -15,27 +31,64 @@ import eu.scape_project.planning.model.User;
 @SessionScoped
 public class GravatarHelper implements Serializable {
 
-    private static final long serialVersionUID = -2943870846396992143L;
+	private static final long serialVersionUID = -2943870846396992143L;
 
-    public static final int DEFAULT_SIZE = 30;
+	public static final int DEFAULT_SIZE = 30;
 
-    @Inject
-    private User user;
+	@Inject
+	private User user;
 
-    public String getGravatarURL() {
-        return getGravatarURL(user.getEmail());
-    }
+	/**
+	 * Returns a URL to the gravatar of the current user.
+	 * 
+	 * @return the URL
+	 */
+	public String getGravatarURL() {
+		return getGravatarURL(user.getEmail());
+	}
 
-    public String getGravatarURL(int size) {
-        return getGravatarURL(user.getEmail(), size);
-    }
+	/**
+	 * Returns a URL to the gravatar of the current user for the provided size.
+	 * 
+	 * @param size
+	 *            the size of the gravtar
+	 * @return the URL
+	 */
+	public String getGravatarURL(int size) {
+		return getGravatarURL(user.getEmail(), size);
+	}
 
-    public String getGravatarURL(String email) {
-        return getGravatarURL(email, DEFAULT_SIZE);
-    }
+	/**
+	 * Returns a URL to the gravatar for the provided email.
+	 * 
+	 * @param email
+	 *            the email of the user
+	 * @return the RUL
+	 */
+	public String getGravatarURL(String email) {
+		return getGravatarURL(email, DEFAULT_SIZE);
+	}
 
-    public String getGravatarURL(String email, int size) {
-        return new Gravatar().setSize(size).setHttps(true).setRating(Rating.GENERAL_AUDIENCE)
-            .setStandardDefaultImage(DefaultImage.MYSTERY_MAN).getUrl(email);
-    }
+	/**
+	 * Returns a URL to the gravatar for the provided email and size.
+	 * 
+	 * @param email
+	 *            the email of the user
+	 * @param size
+	 *            the size of the gravatar
+	 * @return the URL
+	 */
+	public String getGravatarURL(String email, int size) {
+		if (email == null) {
+			return new Gravatar().setSize(size).setHttps(true)
+					.setRating(Rating.GENERAL_AUDIENCE)
+					.setStandardDefaultImage(DefaultImage.MYSTERY_MAN)
+					.getUrl("");
+		}
+
+		return new Gravatar().setSize(size).setHttps(true)
+				.setRating(Rating.GENERAL_AUDIENCE)
+				.setStandardDefaultImage(DefaultImage.MYSTERY_MAN)
+				.getUrl(email);
+	}
 }
