c640443fead67917669d1a1a796abcfcd19dae83
Markus Plangg <plangg@ifs.tuwien.ac.at>
2012-08-31 17:58:33 +0200

    Added reading of results

--- planning-core/src/main/java/eu/scape_project/planning/taverna/executor/SSHTavernaExecutor.java
+++ planning-core/src/main/java/eu/scape_project/planning/taverna/executor/SSHTavernaExecutor.java
@@ -3,12 +3,13 @@ package eu.scape_project.planning.taverna.executor;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Properties;
@@ -25,10 +26,13 @@ import net.schmizz.sshj.xfer.FileSystemFile;
 import net.schmizz.sshj.xfer.InMemorySourceFile;
 import net.sf.taverna.t2.baclava.DataThing;
 import net.sf.taverna.t2.baclava.factory.DataThingFactory;
+import net.sf.taverna.t2.baclava.factory.DataThingXMLFactory;
 
 import org.jdom.Document;
 import org.jdom.Element;
+import org.jdom.JDOMException;
 import org.jdom.Namespace;
+import org.jdom.input.SAXBuilder;
 import org.jdom.output.Format;
 import org.jdom.output.XMLOutputter;
 import org.slf4j.Logger;
@@ -53,6 +57,10 @@ public class SSHTavernaExecutor {
 
     private Properties sshProperties;
 
+    private Integer commandTimeout;
+
+    private HashSet<String> createdDirs = new HashSet<String>();
+
     private String workflowUri;
 
     private File workflowFile;
@@ -71,12 +79,13 @@ public class SSHTavernaExecutor {
     public void init() {
         try {
             sshProperties = PropertiesLoader.loadProperties(SSH_PROPERTIES);
+            commandTimeout = Integer.parseInt(sshProperties.getProperty("command.timeout"));
         } catch (IOException e) {
             log.error("Error loading properties " + SSH_PROPERTIES, e);
         }
     }
 
-    public void execute() throws IOException {
+    public void execute() throws IOException, TavernaExecutorException {
 
         prepareClient();
         KeyProvider kp = getKeyProvider();
@@ -124,15 +133,17 @@ public class SSHTavernaExecutor {
         return kp;
     }
 
-    private void prepare() throws IOException {
+    private void prepare() throws IOException, TavernaExecutorException {
         outputDocPath = tempDir + File.separator + OUTPUT_DOC_FILENAME;
         inputDocPath = prepareInputs();
         workflowPath = prepareWorkflow();
     }
 
-    private String prepareInputs() throws IOException {
+    private String prepareInputs() throws IOException, TavernaExecutorException {
         Element rootElement = new Element("dataThingMap", namespace);
         Document document = new Document(rootElement);
+
+        createdDirs.clear();
 
         for (Entry<TavernaPort, Object> entry : inputData.entrySet()) {
             TavernaPort port = entry.getKey();
@@ -142,8 +153,8 @@ public class SSHTavernaExecutor {
             Object value = entry.getValue();
             if (value instanceof File) {
                 dereferencedInput = addInput(port, (File) value);
-            } else if (value instanceof Collection) {
-                dereferencedInput = addInput(port, (Collection) value);
+            } else if (value instanceof Collection<?>) {
+                dereferencedInput = addInput(port, (Collection<?>) value);
             } else {
                 dereferencedInput = addInput(port, value);
             }
@@ -159,11 +170,18 @@ public class SSHTavernaExecutor {
         XMLOutputter xo = new XMLOutputter(Format.getPrettyFormat());
         // PrintWriter out = new PrintWriter(new FileWriter(inputFile));
         ByteArrayOutputStream out = new ByteArrayOutputStream();
-        xo.output(document, out);
+        try {
+            xo.output(document, out);
+            InputStream in = new ByteArrayInputStream(out.toByteArray());
+            try {
+                return uploadFile(new InputBaclavaFile(INPUT_DOC_FILENAME, in, out.toByteArray().length), tempDir);
+            } finally {
+                in.close();
+            }
+        } finally {
+            out.close();
+        }
 
-        InputStream in = new ByteArrayInputStream(out.toByteArray());
-
-        return uploadFile(new InputBaclavaFile(INPUT_DOC_FILENAME, in, out.toByteArray().length), tempDir);
     }
 
     private Object addInput(TavernaPort port, Collection<? extends Object> objects) throws IOException {
@@ -178,53 +196,93 @@ public class SSHTavernaExecutor {
         return object.toString();
     }
 
-    private Object addInput(TavernaPort port, File file) throws IOException {
-        return uploadFile(file, tempDir);
+    private Object addInput(TavernaPort port, File file) throws IOException, TavernaExecutorException {
+        return uploadFile(file, tempDir + File.separator + port.getName());
     }
 
-    private String createTmpDir() throws IOException {
+    private String createTmpDir() throws IOException, TavernaExecutorException {
         final Session session = ssh.startSession();
         try {
             final Command cmd = session.exec("mktemp -d plato.XXXXXXXXXXXXXXXXXXXX");
             String tempDir = IOUtils.readFully(cmd.getInputStream()).toString();
             cmd.join(5, TimeUnit.SECONDS);
-            tempDir = tempDir.trim();
-            log.debug("Created temporary directory " + tempDir);
-            return tempDir;
+            if (cmd.getExitStatus().equals(0)) {
+                tempDir = tempDir.trim();
+                log.debug("Created temporary directory " + tempDir);
+                return tempDir;
+            } else {
+                String stderr = IOUtils.readFully(cmd.getErrorStream()).toString();
+                log.error("Error creating temporary directory " + stderr);
+                throw new TavernaExecutorException("Error creating temporary directory " + stderr);
+            }
         } finally {
             session.close();
         }
     }
 
-    private String uploadFile(File file, String tempDir) throws IOException {
-        String targetPath = tempDir + File.separator + file.getName();
+    private String uploadFile(File file, String dir) throws IOException, TavernaExecutorException {
+        String targetPath = dir + File.separator + file.getName();
+        if (!createdDirs.contains(dir)) {
+            createUploadDir(dir);
+        }
         ssh.newSCPFileTransfer().upload(new FileSystemFile(file), targetPath);
         log.debug("Uploaded file " + file.getAbsolutePath() + " to " + targetPath);
         return targetPath;
     }
 
-    private String uploadFile(InMemorySourceFile file, String tempDir) throws IOException {
-        String targetPath = tempDir + File.separator + file.getName();
+    private String uploadFile(InMemorySourceFile file, String dir) throws IOException, TavernaExecutorException {
+        String targetPath = dir + File.separator + file.getName();
+        if (!createdDirs.contains(dir)) {
+            createUploadDir(dir);
+        }
         ssh.newSCPFileTransfer().upload(file, targetPath);
         log.debug("Uploaded file " + file.getName() + " to " + targetPath);
         return targetPath;
     }
 
-    private void executeWorkflow() throws IOException {
+    private void createUploadDir(String dir) throws IOException, TavernaExecutorException {
+        final Session session = ssh.startSession();
+        try {
+            final Command cmd = session.exec("mkdir -p \"" + dir + "\"");
+            cmd.join(5, TimeUnit.SECONDS);
+
+            if (cmd.getExitStatus().equals(0)) {
+                log.debug("Created directory " + dir);
+                createdDirs.add(dir);
+            } else {
+                String stderr = IOUtils.readFully(cmd.getErrorStream()).toString();
+                log.error("Error creating directory " + dir + ": " + stderr);
+                throw new TavernaExecutorException("Error creating directory " + dir + ": " + stderr);
+            }
+
+        } finally {
+            session.close();
+        }
+    }
+
+    private void executeWorkflow() throws IOException, TavernaExecutorException {
         final Session session = ssh.startSession();
         try {
             String command = TAVERNA_COMMAND.replace("%%inputdoc%%", inputDocPath)
                 .replace("%%outputdoc%%", outputDocPath).replace("%%workflow%%", workflowPath);
             final Command cmd = session.exec(command);
-            String tempDir = IOUtils.readFully(cmd.getInputStream()).toString();
-            cmd.join(5, TimeUnit.SECONDS);
+            cmd.join(commandTimeout, TimeUnit.SECONDS);
+
+            if (cmd.getExitStatus().equals(0)) {
+                log.info("Executed workflow");
+            } else {
+                String stderr = IOUtils.readFully(cmd.getErrorStream()).toString();
+                log.error("Error executing workflow: " + stderr);
+                throw new TavernaExecutorException("Error executing workflow: " + stderr);
+            }
+
             log.debug("Executed workflow with command " + command);
         } finally {
             session.close();
         }
     }
 
-    private String prepareWorkflow() throws IOException {
+    private String prepareWorkflow() throws IOException, TavernaExecutorException {
         if (workflowFile != null) {
             return uploadFile(workflowFile, tempDir);
         } else if (workflowUri != null && !workflowUri.equals("")) {
@@ -233,6 +291,36 @@ public class SSHTavernaExecutor {
             // TODO: Exception
             return null;
         }
+    }
+
+    private void getResults() throws IOException, TavernaExecutorException {
+
+        Map<TavernaPort, Object> ouputData = new HashMap<TavernaPort, Object>();
+
+        File outputDoc = File.createTempFile("ssh-taverna-executor", ".xml");
+        downloadFile(OUTPUT_DOC_FILENAME, outputDoc);
+
+        SAXBuilder builder = new SAXBuilder();
+        FileInputStream is = new FileInputStream(outputDoc);
+        try {
+            Document inputDoc = builder.build(is);
+            Map<String, DataThing> outputPorts = DataThingXMLFactory.parseDataDocument(inputDoc);
+
+            for (Entry<String, DataThing> entry : outputPorts.entrySet()) {
+
+            }
+
+        } catch (JDOMException e) {
+            throw new TavernaExecutorException("Error reading output document", e);
+        } finally {
+            is.close();
+        }
+
+    }
+
+    private void downloadFile(String path, File localFile) throws IOException {
+        ssh.newSCPFileTransfer().download(tempDir + path, new FileSystemFile(localFile));
+        log.debug("Downloaded file " + path + " to " + localFile.getAbsolutePath());
     }
 
     // --------------- getter/setter ---------------
@@ -267,14 +355,6 @@ public class SSHTavernaExecutor {
     public void setOutputData(Map<TavernaPort, Object> outputData) {
         this.outputData = outputData;
     }
-
-    // private void downloadFile(SSHClient ssh, File file, String tempDir)
-    // throws IOException {
-    // ssh.newSCPFileTransfer().upload(new FileSystemFile(file), tempDir +
-    // file.getName());
-    // log.debug("Uploaded file " + file.getAbsolutePath() + " to " + tempDir +
-    // file.getName());
-    // }
 
     private class InputBaclavaFile extends InMemorySourceFile {
 
---
+++ planning-core/src/main/java/eu/scape_project/planning/taverna/executor/TavernaExecutorException.java
@@ -0,0 +1,21 @@
+package eu.scape_project.planning.taverna.executor;
+
+public class TavernaExecutorException extends Exception {
+
+    public TavernaExecutorException() {
+        super();
+    }
+
+    public TavernaExecutorException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public TavernaExecutorException(String message) {
+        super(message);
+    }
+
+    public TavernaExecutorException(Throwable cause) {
+        super(cause);
+    }
+
+}
