23d5d259fe1c8f165145a97ed5e8eb41a63d004b
Michael Kraxner <kraxner@ifs.tuwien.ac.at>
2012-11-22 17:43:58 +0100

    unified pom versioning, integration test experiments

--- plato/src/test/java/eu/scape_project/planning/plato/wf/FullPlanningworkflowIT.java
+++ plato/src/test/java/eu/scape_project/planning/plato/wf/FullPlanningworkflowIT.java
@@ -1,9 +1,13 @@
 package eu.scape_project.planning.plato.wf;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
+import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 
 import org.jboss.arquillian.container.test.api.Deployment;
@@ -11,10 +15,14 @@ import org.jboss.arquillian.container.test.api.Testable;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Node;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.exporter.ArchiveExportException;
 import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.impl.base.exporter.zip.ZipExporterImpl;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.jboss.shrinkwrap.resolver.api.maven.MavenResolverSystem;
+import org.jboss.shrinkwrap.resolver.api.maven.PomEquippedResolveStage;
 import org.jboss.weld.context.bound.Bound;
 import org.jboss.weld.context.bound.BoundConversationContext;
 import org.jboss.weld.context.bound.MutableBoundRequest;
@@ -24,10 +32,12 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import eu.scape_project.planning.application.Mock;
 import eu.scape_project.planning.application.MockAuthenticatedUserProvider;
+import eu.scape_project.planning.exception.PlanningException;
+import eu.scape_project.planning.manager.PlanManager;
 import eu.scape_project.planning.model.Plan;
-import eu.scape_project.planning.plato.wfview.full.CreatePlanView;
+import eu.scape_project.planning.model.User;
+import eu.scape_project.planning.validation.ValidationError;
 
 @RunWith(Arquillian.class)
 public class FullPlanningworkflowIT {
@@ -38,10 +48,40 @@ public class FullPlanningworkflowIT {
     @Bound
     private BoundConversationContext conversationContext;
     
-    @Inject
-    private CreatePlanView createPlanView;
+    @Inject private Instance<DefineBasis> defineBasisSource;
+    
+    @Inject private User user;
+    
+    @Inject private PlanManager planManager;
 
     @Deployment
+    public static WebArchive createSlimDeployment() throws ArchiveExportException, IllegalArgumentException, FileNotFoundException {
+        PomEquippedResolveStage pomResolver = Maven.resolver().loadPomFromFile("../pom.xml", "pom.xml");
+        
+        File[] platoModelJars = pomResolver.resolve("eu.scape-project.pw:plato-model").withoutTransitivity().asFile();
+        File[] libs = pomResolver
+            .resolve("eu.scape-project.pw:planning-core").withTransitivity().asFile();
+        
+        JavaArchive platoModel = ShrinkWrap.createFromZipFile(JavaArchive.class, platoModelJars[0]);
+        platoModel.addAsResource("test-persistence.xml", "META-INF/persistence.xml");
+        
+        
+        
+        WebArchive archive = ShrinkWrap.create(WebArchive.class)
+            .addAsLibraries(libs)
+            .addAsResource("slimtest-persistence.xml","persistence.xml")
+            .addPackage("eu.scape_project.planning.plato")
+            .addPackage("eu.scape_project.planning.policies")
+            .addPackage("eu.scape_project.planning.user")
+            .addPackage("eu.scape_project.planning.utils")
+            .addClass(FullPlanningworkflowIT.class);
+        
+        ZipExporterImpl exporter = new ZipExporterImpl(archive);
+        exporter.exportTo(new FileOutputStream("../test-archive.war"));
+        return archive;
+    }
+    
+    //@Deployment
     public static EnterpriseArchive createDeployment() {
 
         EnterpriseArchive planningsuiteEar = ShrinkWrap.createFromZipFile(EnterpriseArchive.class, new File(
@@ -82,20 +122,35 @@ public class FullPlanningworkflowIT {
     }
     
     @Test
-    public void test() {
+    public void test() throws PlanningException {
+            log.info("entering test");
 
             conversationContext.associate(new MutableBoundRequest(
                             new HashMap<String, Object>(), new HashMap<String, Object>()));
             conversationContext.activate();
-
-            Assert.assertNotNull("Viewbean not injected", createPlanView);
             
-            Assert.assertNull(createPlanView.getPlan());
-            createPlanView.createPlan();
-            Plan plan = createPlanView.getPlan();
-            Assert.assertNotNull(plan);
+            Assert.assertTrue(conversationContext.isActive());
+            
+            DefineBasis defineBasis = defineBasisSource.get();
+            Assert.assertNotNull(defineBasis);
+
+            Plan plan = new Plan();
             plan.getPlanProperties().setName("Test Plan");
-            createPlanView.savePlan();
+            plan.getPlanProperties().setAuthor(user.getUsername());
+            plan.getPlanProperties().setOwner(user.getUsername());
+//            planManager.store(plan);
+            defineBasis.setPlan(plan);
+            Assert.assertNotNull(defineBasis.getPlan());
+            
+            defineBasis.save();
+            log.info("stored plan");
+            List<ValidationError> errors = new ArrayList<ValidationError>();
+            Assert.assertFalse(defineBasis.proceed(errors));
+            Assert.assertTrue(errors.size() != 0);
+            for (ValidationError ve: errors) {
+                log.debug(ve.getMessage());
+            }
+            
     }
     
 }
