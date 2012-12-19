c11e2eee1783513f20cc8126b3f4a0ff9da4ce38
Michael Kraxner <kraxner@ifs.tuwien.ac.at>
2012-12-19 13:56:36 +0100

    adds test for relation samplerecordsdefinition-samples

--- plato-model/src/test/java/eu/scape_project/planning/model/ElementOrderPersistenceTest.java
+++ plato-model/src/test/java/eu/scape_project/planning/model/ElementOrderPersistenceTest.java
@@ -22,8 +22,9 @@ public class ElementOrderPersistenceTest {
     }
     
     
+    @SuppressWarnings("unchecked")
     @Test
-    public void storeSampeRecordsDefinition(){
+    public void checkRelationSampleRecordsDefinitionToSampleObjects(){
         SampleRecordsDefinition samplesDef = new SampleRecordsDefinition();
         SampleObject s1 = new SampleObject("s1");
         SampleObject s2 = new SampleObject("s2");
@@ -33,17 +34,70 @@ public class ElementOrderPersistenceTest {
         samplesDef.addRecord(s2);
         samplesDef.addRecord(s3);
         
+        em.getTransaction().begin();
+        
         em.persist(samplesDef);
         
-        List<SampleObject> samples = em.createQuery("select from SampleObject").getResultList();
+        em.refresh(samplesDef);
+        
+        // check overall existing samples
+        List<SampleObject> samples = em.createQuery("select s from SampleObject s").getResultList();
         Assert.assertNotNull(samples);
         Assert.assertEquals(3, samples.size());
+        
+        // reload and check persisted samplerecordsdefinition
+        SampleRecordsDefinition sDef2 = em.find(SampleRecordsDefinition.class, samplesDef.getId());
+        // check proper order of samples
+        Assert.assertEquals(s1.getShortName(), sDef2.getRecords().get(0).getShortName());
+        Assert.assertEquals(s2.getShortName(), sDef2.getRecords().get(1).getShortName());
+        Assert.assertEquals(s3.getShortName(), sDef2.getRecords().get(2).getShortName());
 
-        Assert.assertSame(s1.getShortName(), samples.get(0).getShortName());
-        Assert.assertSame(s2.getShortName(), samples.get(1).getShortName());
-        Assert.assertSame(s3.getShortName(), samples.get(2).getShortName());
+        em.getTransaction().commit();
+        
+        // now remove the sample in between 
+        em.getTransaction().begin();
+        sDef2.removeRecord(s2);
+        // and add a new one
+        SampleObject s4 = new SampleObject("s4");
+        sDef2.addRecord(s4);
+        em.persist(sDef2);
+        
+        // check overall existing samples
+        samples = em.createQuery("select s from SampleObject s").getResultList();
+        Assert.assertNotNull(samples);
+        Assert.assertEquals("orphanRemoval should take care of removing sample s2.", 3, samples.size()); 
+
+        // reload and check proper order of samples
+        sDef2 = em.find(SampleRecordsDefinition.class, samplesDef.getId());
+        Assert.assertEquals(s1.getShortName(), sDef2.getRecords().get(0).getShortName());
+        Assert.assertEquals(s3.getShortName(), sDef2.getRecords().get(1).getShortName());
+        Assert.assertEquals(s4.getShortName(), sDef2.getRecords().get(2).getShortName());
+        
+        em.getTransaction().commit();
+        // test rollback
+        em.getTransaction().begin();
+        sDef2.removeRecord(s1);
+        sDef2.addRecord(new SampleObject("s5"));
+        em.persist(sDef2);
+        em.getTransaction().rollback();
+
+        em.getTransaction().begin();
+
+        // reload and check proper order of samples
+        sDef2 = em.merge(sDef2);// = em.find(SampleRecordsDefinition.class, samplesDef.getId());
+        em.refresh(sDef2);
+        Assert.assertEquals(s1.getShortName(), sDef2.getRecords().get(0).getShortName());
+        Assert.assertEquals(s3.getShortName(), sDef2.getRecords().get(1).getShortName());
+        Assert.assertEquals(s4.getShortName(), sDef2.getRecords().get(2).getShortName());
+        
+        sDef2.removeRecord(s1);
+        sDef2.addRecord(new SampleObject("s5"));
+        sDef2.removeRecord(s3);
+        
+        sDef2 = em.merge(sDef2);
+        em.getTransaction().commit();
+        sDef2 = em.find(SampleRecordsDefinition.class, samplesDef.getId());
+        Assert.assertEquals(s4.getShortName(), sDef2.getRecords().get(0).getShortName());
+        Assert.assertEquals("s5", sDef2.getRecords().get(1).getShortName());
     }
-    
-    
-
 }
