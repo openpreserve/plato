4e57dbbad3e6e0ace5cf5582d2d4c12ab14b9923
Michael Kraxner <kraxner@ifs.tuwien.ac.at>
2013-01-22 14:07:04 +0100

    fixes plato-model integration tests, adds setup script for DB

--- tools/setup-database.sh
+++ tools/setup-database.sh
@@ -2,6 +2,8 @@
 MPASS="$1"
 PLATO_PASS="$2"
 IDP_PASS="$3"
+DB_URL="$4"
+
 
 VERSION="7.1.0"
  
@@ -14,16 +16,23 @@ MPASSOPT="-p"$MPASS
 
 if [ $# -lt 3 ]
 then
-	echo "Usage: $0 {MySQL-Root Pwd} {Plato DB Pwd} {IDP DB Pwd}"
-	echo "Creating Plato and IDP databases and users"
+	echo "Usage: $0 {MySQL-Root Pwd} {Plato DB Pwd} {IDP DB Pwd} {DB URL}]"
+	echo " Creates users and databases for Plato and IDP"
+	echo " - if no DB URL is provided 'localhost' is used "
 	exit 1
 fi
 
-echo standalone-$VERSION-template.xml
+if [ "test$DB_URL" == "test" ]
+then
+	DB_URL="localhost"
+fi
+
+echo  "Generating configuration file for JBoss AS 7-$VERSION: standalone.xml"
 
 cat standalone-$VERSION-template.xml | \
-sed -e 's/{PLATO_PWD}/'$PLATO_PASS'/g' -e's/{IDP_PWD}/'$IDP_PASS'/g' > standalone.xml
+sed -e 's/{PLATO_PWD}/'$PLATO_PASS'/g' -e's/{IDP_PWD}/'$IDP_PASS'/g' -e's/{DB_URL}/'$DB_URL'/g' > standalone.xml
 
+echo "Creating databases and users..."
 $MYSQL -u root -p$MPASS -e "create user 'plato'@'localhost' identified by '"$PLATO_PASS"';"
 $MYSQL -u root -p$MPASS -e "create database platodb;"
 $MYSQL -u root -p$MPASS -e "grant all privileges on platodb.* to 'plato'@'localhost';"
@@ -32,7 +41,11 @@ $MYSQL -u root -p$MPASS -e "create user 'idp'@'localhost' identified by '"$IDP_P
 $MYSQL -u root -p$MPASS -e "create database idpdb;"
 $MYSQL -u root -p$MPASS -e "grant all privileges on idpdb.* to 'idp'@'localhost';"
 
+echo "Creating databases and users for tests..."
 $MYSQL -u root -p$MPASS -e "create user 'platotest'@'localhost' identified by 'platotest';"
 $MYSQL -u root -p$MPASS -e "create database platodbtest;"
 $MYSQL -u root -p$MPASS -e "grant all privileges on platodbtest.* to 'platotest'@'localhost';"
 
+$MYSQL -u root -p$MPASS -e "create user 'idptest'@'localhost' identified by 'idptest';"
+$MYSQL -u root -p$MPASS -e "create database idpdbtest;"
+$MYSQL -u root -p$MPASS -e "grant all privileges on idpdbtest.* to 'idptest'@'localhost';"
