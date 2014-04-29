12bddb08ddd836406e7ff67de12fdcb78a4c77bb
Michael Kraxner <kraxner@ifs.tuwien.ac.at>
2014-04-29 17:47:31 +0200

    updates vagrant config, fixes jboss-util pom * updates vagrant config - it runs through and server is started   but some improvements are necessary, as stated in the bootstrap.sh * jboss-util/pom.xml specifies the required dk version (to accept   annotations)

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,19 @@
 #!/usr/bin/env bash
+apt-get update
 
-JBOSS_HOME=/usr/local/share/jboss
-export JBOSS_HOME
-
-sudo apt-get update
-
-sudo apt-get install -y openjdk-7-jdk
+apt-get install -y openjdk-7-jdk
 # update-alternatives --set javac /usr/lib/jvm/java-7-openjdk/bin/javac
 
-sudo echo "JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64" >> /etc/environment
+echo "JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64" >> /etc/environment
+echo "JBOSS_HOME=/usr/local/share/jboss" >> /etc/environment
 
-sudo source /etc/environment
+source /etc/environment
 
 echo "Setting up mysql..."
-
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password plato123'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password plato123'
-sudo apt-get -y install mysql-server
-
-
+# note that the database passwort is required further down when preparing jboss configuration
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password plato123'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password plato123'
+apt-get -y install mysql-server
 
 
 # in the Server Section [mysqld] 
@@ -35,49 +30,58 @@ sed -i.utf8.bak -e 's/\[mysqld\]/\[mysqld\]\ncharacter-set-server=utf8\ncollatio
 /etc/init.d/mysql restart
 
 
-echo "Setting up Plato"
+echo "Setting up Plato..."
 
 apt-get install -y git 
 apt-get install -y maven
 
+# TODO: do this as user vagrant, in its home folder, not in tmp, to have a better duick-dev environment
+# su vagrant
 cd /tmp
 git clone https://github.com/openplanets/plato.git
 cd plato
 git checkout vagrant-dev
 
-
+# exit
 wget http://download.jboss.org/jbossas/7.1/jboss-as-7.1.0.Final/jboss-as-7.1.0.Final.tar.gz
 tar xfvz jboss-as-7.1.0.Final.tar.gz
 mv jboss-as-7.1.0.Final $JBOSS_HOME
 
+# add mysql drivers as jboss module
 mkdir -p $JBOSS_HOME/modules/com/mysql/main
 cp /tmp/plato/provisional/mysql/* $JBOSS_HOME/modules/com/mysql/main/
 
+# update picketlink module 
 rm -r $JBOSS_HOME/modules/org/picketlink/main/*
 cp /tmp/plato/provisional/picketlink/* $JBOSS_HOME/modules/org/picketlink/main/
 
+# add utf8 valve as jboss module
 mkdir -p $JBOSS_HOME/modules/eu/scape_project/planning/util/
 cd /tmp/plato/jboss-util
 mvn package
-cp target/*.jar  $JBOSS_HOME/modules/eu/scape_project/planning/util/
-cp target/classes/module.xml  $JBOSS_HOME/modules/eu/scape_project/planning/util/
+cp target/*.jar  $JBOSS_HOME/modules/eu/scape_project/planning/util/main
+cp target/classes/module.xml  $JBOSS_HOME/modules/eu/scape_project/planning/util/main
 
+# prepare jboss config for database: TODO: move to vagrant home folder
 cd /tmp/plato/tools
 ./setup-database.sh plato123 plato idp
 
 cp standalone.xml $JBOSS_HOME/standalone/configuration/
 
-# adduser appserver
-# chown -R appserver $JBOSS_HOME
 
-
+# finally build planning suite TODO: move to vagrant home folder
 cd /tmp/plato
 mvn clean install -DskipTests
 
-
+# ... and deploy web apps to jboss
 cp /tmp/plato/idp/target/idp.war $JBOSS_HOME/standalone/deployments/
 cp /tmp/plato/planningsuite-ear/target/planningsuite-ear.ear $JBOSS_HOME/standalone/deployments/
 
-# su -c "nohup $JBOSS_HOME/bin/standalone.sh -b=0.0.0.0 &" - appserver
+# we do not want to run jboss as root (better: more fine grained permissions: log/data/deployments)
+chown -R vagrant $JBOSS_HOME
 
-nohup $JBOSS_HOME/bin/standalone.sh -b=0.0.0.0 &
+# # su -c "nohup $JBOSS_HOME/bin/standalone.sh -b=0.0.0.0 &" - appserver
+# 
+su -c "nohup $JBOSS_HOME/bin/standalone.sh -b=0.0.0.0 &" - vagrant
+
+tail -f $JBOSS_HOME/standalone/log/server.log
