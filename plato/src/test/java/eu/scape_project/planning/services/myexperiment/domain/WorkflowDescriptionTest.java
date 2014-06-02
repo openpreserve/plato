1abd0e68c53e76a562b9f96e13083ed09bee9180
Markus Plangg <plangg@ifs.tuwien.ac.at>
2014-06-02 14:54:36 +0200

    Added accepted mimetypes to service output, Added tests for QA workflow description

--- plato/src/test/java/eu/scape_project/planning/services/myexperiment/domain/WorkflowDescriptionTest.java
+++ plato/src/test/java/eu/scape_project/planning/services/myexperiment/domain/WorkflowDescriptionTest.java
@@ -36,7 +36,7 @@ import eu.scape_project.planning.services.myexperiment.domain.Port.PredefinedPar
 public class WorkflowDescriptionTest {
 
     @Test
-    public void unmarshallWorkflowDescription() throws JAXBException, URISyntaxException {
+    public void unmarshallWorkflowDescription_migration() throws JAXBException, URISyntaxException {
         JAXBContext context = JAXBContext.newInstance(WorkflowInfo.class, WorkflowDescription.class);
         Unmarshaller unmarshaller = context.createUnmarshaller();
 
@@ -127,5 +127,75 @@ public class WorkflowDescriptionTest {
         Assert.assertEquals("target_path", outputPorts.get(1).getName());
         Assert.assertEquals("Path to the migration target", outputPorts.get(1).getDescription());
         Assert.assertEquals(ComponentConstants.VALUE_TARGET_OBJECT, outputPorts.get(1).getValue());
+    }
+
+    @Test
+    public void unmarshallWorkflowDescription_qa() throws JAXBException, URISyntaxException {
+        JAXBContext context = JAXBContext.newInstance(WorkflowInfo.class, WorkflowDescription.class);
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+
+        InputStream in = Thread.currentThread().getContextClassLoader()
+            .getResourceAsStream("myexperiment/workflow-description-qa.xml");
+
+        WorkflowDescription wf = (WorkflowDescription) unmarshaller.unmarshal(in);
+        wf.readMetadata();
+
+        Assert.assertEquals(new URI("http://www.myexperiment.org/workflow.xml?id=4246"), wf.getUri());
+        Assert.assertEquals(new URI("http://www.myexperiment.org/workflows/4246/versions/3"), wf.getResource());
+
+        Assert.assertEquals("4246", wf.getId());
+        Assert.assertEquals("3", wf.getVersion());
+        Assert.assertEquals("Detect similarity in images using peak absolute error", wf.getName());
+        Assert.assertEquals("SCAPE QA Object Component", wf.getDescription());
+        Assert
+            .assertEquals(
+                "http://www.myexperiment.org/workflows/4246/download/detect_similarity_in_images_using_peak_absolute_error_725586.t2flow?version=3",
+                wf.getContentUri());
+        Assert.assertEquals("application/vnd.taverna.t2flow+xml", wf.getContentType());
+
+        Assert.assertNotNull(wf.getType());
+        Assert.assertNotNull(wf.getUploader());
+        Assert.assertEquals("http://www.myexperiment.org/workflows/4246/versions/3/previews/full", wf.getPreview());
+        Assert.assertEquals("http://www.myexperiment.org/workflows/4246/versions/3/previews/svg", wf.getSvg());
+        Assert.assertNotNull(wf.getLicenseType());
+
+        Assert.assertEquals(1, wf.getTags().size());
+        Assert.assertEquals(0, wf.getRatings().size());
+
+        Assert.assertEquals("http://purl.org/DP/components#QAObjectComparison", wf.getProfile());
+
+        List<AcceptedMimetypes> acceptedMimetypes = wf.getAcceptedMimetypes();
+        Assert.assertEquals(1, acceptedMimetypes.size());
+        Assert.assertEquals("image/*", acceptedMimetypes.get(0).getLeftMimetype());
+        Assert.assertEquals("image/*", acceptedMimetypes.get(0).getRightMimetype());
+
+        List<String> acceptedMimetype = wf.getAcceptedMimetype();
+        Assert.assertEquals(1, acceptedMimetype.size());
+        Assert.assertEquals("image/tiff", acceptedMimetype.get(0));
+
+        List<Installation> installations = wf.getInstallations();
+        Assert.assertEquals(1, installations.size());
+        Assert.assertEquals("http://purl.org/DP/components#Debian", installations.get(0).getEnvironment());
+        List<Dependency> dependencies = installations.get(0).getDependencies();
+        Assert.assertEquals(2, dependencies.size());
+        Assert.assertEquals("dcraw", dependencies.get(0).getName());
+        Assert.assertEquals("http://opensource.org/licenses/GPL-2.0", dependencies.get(0).getLicense());
+        Assert.assertEquals("default-jre", dependencies.get(1).getName());
+        Assert.assertEquals("http://opensource.org/licenses/GPL-2.0", dependencies.get(1).getLicense());
+
+        List<Port> inputPorts = wf.getInputPorts();
+        Assert.assertEquals(2, inputPorts.size());
+        Assert.assertEquals("leftimage", inputPorts.get(0).getName());
+        Assert.assertEquals("Path to the left image", inputPorts.get(0).getDescription());
+        Assert.assertEquals(ComponentConstants.VALUE_LEFT_OBJECT, inputPorts.get(0).getValue());
+
+        Assert.assertEquals("rightimage", inputPorts.get(1).getName());
+        Assert.assertEquals("Path to the right image", inputPorts.get(1).getDescription());
+        Assert.assertEquals(ComponentConstants.VALUE_RIGHT_OBJECT, inputPorts.get(1).getValue());
+
+        List<Port> outputPorts = wf.getOutputPorts();
+        Assert.assertEquals(1, outputPorts.size());
+        Assert.assertEquals("PAE", outputPorts.get(0).getName());
+        Assert.assertEquals("http://purl.org/DP/quality/measures#7", outputPorts.get(0).getValue());
     }
 }
